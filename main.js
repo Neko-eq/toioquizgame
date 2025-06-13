bc96de6e916e35d71735b1929f12ac699b5f7951
c3230735 <silver.bullet0306@gmail.com>
2025-06-13 19:44:52 +0900

    js up

--- main.js
+++ main.js
@@ -1,32 +1,8 @@
-import { CoreCube } from './index.js';
-
-
+// main.js
 let cube;
-let virtualX = 180;
-let virtualY = 180;
-
-function updateToioIcon() {
-  const toioElem = document.getElementById("toio");
-  toioElem.style.left = virtualX + "px";
-  toioElem.style.top = virtualY + "px";
-}
 
 document.getElementById("connect").addEventListener("click", async () => {
-  try {
-    cube = new CoreCube();
-    await cube.connect();
-
-    alert("toioに接続しました");
-
-    // 接続後、イベントリスナーを登録
-    cube.onButtonPress(() => {
-      console.log("ボタンが押されました");
-      virtualY -= 10;
-      updateToioIcon();
-    });
-
-  } catch (e) {
-    console.error("接続エラー", e);
-    alert("toioとの接続に失敗しました");
-  }
+  cube = new CoreCube(); // グローバルに定義されている
+  await cube.connect();
+  alert("接続しました！");
 });
