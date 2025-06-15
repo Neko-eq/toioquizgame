28d99e6c71578f96516fc8a045ba15de77f7eb94
c3230735 <silver.bullet0306@gmail.com>
2025-06-15 10:54:50 +0900

    a

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ document.getElementById("connect").addEventListener("click", async () => {
     console.log("接続開始...");
 
     const device = await navigator.bluetooth.requestDevice({
-      acceptAllDevices: true,
-      optionalServices: [
-        '0000fd6f-0000-1000-8000-00805f9b34fb', // toio制御サービス
-        '0000180f-0000-1000-8000-00805f9b34fb', // Battery
-        '00001800-0000-1000-8000-00805f9b34fb', // Generic Access
-        '00001801-0000-1000-8000-00805f9b34fb'  // Generic Attribute
-      ]
-    });
+  acceptAllDevices: true,
+  optionalServices: [
+    '0000fd6f-0000-1000-8000-00805f9b34fb', // toio制御サービス
+    '0000180f-0000-1000-8000-00805f9b34fb', // Battery
+    '00001800-0000-1000-8000-00805f9b34fb', // Generic Access
+    '00001801-0000-1000-8000-00805f9b34fb'  // Generic Attribute
+  ]
+});
+
 
     const server = await device.gatt.connect();
 
